647efa9e70da64f7361aaff1abe645cfac9356fa
prog-ops <59245989+prog-ops@users.noreply.github.com>
2023-06-07 18:58:19 +0700

    Add files via upload

--- src/screens/LikeApp.js
+++ src/screens/LikeApp.js
@@ -13,35 +13,39 @@ const CounterItem = ({counter, onIncrement, onDecrement}) => (
             source={require('../../assets/1.jpg')}
             style={{
               resizeMode: 'cover',
-              width: SCREEN_WIDTH * 0.8,
+              width: SCREEN_WIDTH * 0.9,
               height: SCREEN_HEIGHT * 0.5,
-              borderTopRightRadius: (SCREEN_HEIGHT * 0.04)/2,
-              borderTopLeftRadius: (SCREEN_HEIGHT * 0.04)/2,
+              borderTopRightRadius: (SCREEN_WIDTH * 0.06) / 2,
+              borderTopLeftRadius: (SCREEN_WIDTH * 0.06) / 2,
             }}
             resizeMode='cover'
         />
       </View>
       <View style={styles.itemContainerButtons}>
-          <View style={[{
-            ...styles.button,
-            backgroundColor: '#363ef3'
-          }]}>
-            <Text style={{
-              fontSize: 20
-            }}>{counter} Like</Text>
-          </View>
-          <TouchableOpacity onPress={onIncrement} style={[{
-            ...styles.button,
-            backgroundColor: '#363ef3'
-          }]}>
-            <Icon name='heart-outline' size={20} color='white'/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onDecrement} style={[{
-            ...styles.button,
-            backgroundColor: '#ff2d00'
-          }]}>
-            <Text style={styles.buttonText}>Dislike</Text>
-          </TouchableOpacity>
+        <View style={[{
+          ...styles.button,
+          backgroundColor: '#fff',
+          elevation: 10
+        }]}>
+          <Text style={{
+            fontSize: 20, color: 'black'
+          }}>{counter} Like</Text>
+        </View>
+        <TouchableOpacity onPress={onIncrement} style={[{
+          ...styles.button,
+          backgroundColor: '#363ef3'
+        }]}>
+          <Icon name='heart-outline' size={20} color='white'/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onDecrement} style={[{
+          ...styles.button,
+          backgroundColor: '#ff2d00'
+        }]}>
+          <Text style={{
+            color: '#fff',
+            fontSize: 20
+          }}>Dislike</Text>
+        </TouchableOpacity>
       </View>
     </View>
 );
@@ -68,9 +72,36 @@ const LikeApp = () => {
   return (
       <View style={styles.container}>
         <View style={styles.topButtonContainer}>
-          <Button style={{borderRadius: 20}} title="Like All" onPress={() => setCounters(counters.map((counter) => counter + 1))}/>
-          <Button style={{borderRadius: 20}} title="Dislike All" onPress={() => setCounters(counters.map((counter) => counter - 1))}/>
-          <Button style={{borderRadius: 20}} title="Reset All" onPress={resetCounters}/>
+          <TouchableOpacity
+              style={{
+                borderRadius: 7,
+                backgroundColor: '#363ef3',
+                paddingHorizontal: 20,
+                paddingVertical: 10,
+              }}
+              onPress={() => setCounters(counters.map((counter) => counter + 1))}>
+            <Text style={{color: 'white'}}>Like All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={{
+                borderRadius: 7,
+                backgroundColor: '#fff',
+                paddingHorizontal: 20,
+                paddingVertical: 10,
+                elevation: 2,
+              }}
+              onPress={resetCounters}><Text>Reset All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={{
+                borderRadius: 7,
+                backgroundColor: '#ff2d00',
+                paddingHorizontal: 20,
+                paddingVertical: 10,
+              }}
+              onPress={() => setCounters(counters.map((counter) => counter - 1))}>
+            <Text style={{color: 'white'}}>Dislike All</Text>
+          </TouchableOpacity>
         </View>
         <FlatList
             data={counters}
@@ -91,6 +122,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    marginTop: 20,
   },
   topButtonContainer: {
     flexDirection: 'row',
@@ -102,14 +134,16 @@ const styles = StyleSheet.create({
     width: SCREEN_WIDTH * 0.9,
     flexDirection: 'column',
     alignItems: 'center',
-    marginBottom: 8,
-    backgroundColor: 'brown',
-    borderRadius: 15
+    marginBottom: 20,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    elevation: 2
   },
   itemContainerButtons: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     marginBottom: 16,
     padding: 12,
   },
@@ -120,9 +154,6 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     justifyContent: 'center',
   },
-  buttonText: {
-    color: '#fff'
-  }
 });
 
 export default LikeApp;
